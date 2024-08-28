b36a28658eb04d4d40d9d12dfa5e7962d4664a2c
divyanshu <divyanshubisht5734@gmail.com>
2024-08-28 12:06:18 +0530

    Calculator

---
+++ javascript.js
@@ -0,0 +1,36 @@
+let userinput = document.querySelector('#bold')
+const ans = document.querySelector('#f')
+let buttons = document.querySelectorAll('.button')
+
+let string = "";
+
+
+Array.from(buttons).forEach( a=>{
+
+    a.addEventListener('click' , (e)=>{
+      
+        if(e.target.innerHTML == '='){
+            string = string.replace('^' , "**");
+            string = eval(string);
+            ans.value = string ;
+            userinput.value = ""; 
+        }else if( e.target.innerHTML == 'AC'){
+
+            string= "";
+            userinput.value = string ;
+            ans.value= string;
+        }else if (e.target.innerHTML == 'DEL'){
+
+             string = string.substring(0 , string.length-1) ;
+
+             userinput.value = string ;
+        }
+        else{
+
+             string += e.target.innerHTML;
+             userinput.value = string ;
+        }
+            
+    });
+
+});
